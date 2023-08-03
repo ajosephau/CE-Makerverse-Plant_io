815714b630f410b21fe49d116a8ff5e707638aa8
Anthony Joseph <ajosephau@users.noreply.github.com>
2023-08-03 10:15:46 +1000

    add code to upload to influxdb

--- code/main.py
+++ code/main.py
@@ -1,33 +1,54 @@
 # This project requires the Plant_io module which handles
-# - Interfacing with electronics (pump, moisture sensor, PiicoDev modules)
+# - Interfacing with electronics (pump, moisture sensor)
 # - Datalogging to a file
 #
-# Plant_io removes a lot of setup complexity from PiicoDev but is only compatible with select modules.
+# This code will run simple automatic irrigation based on soil moisture
 
+import network
+import secrets
+import time
 from time import sleep_ms
+import ntptime
+import urequests
+
 from Plant_io import Plant_io, DataLogger
 
 
 while True:
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(secrets.SSID, secrets.PASSWORD)
+    print(f'Wifi connected? {wlan.isconnected()}')
+
+    ntptime.settime()
+    current_time = time.time()
+    print(f"Current time: {current_time}")
     ### Step 1: Initialise the Plant
     plant = Plant_io()
-    plant.moisture_setpoint = 50 # change this to tune how moist the growing media should be. Use the results from test_moisture_sensor.py
+    plant.moisture_setpoint = 32 # change this to tune how moist the growing media should be. Use the results from test_moisture_sensor.py
     
     # Attach sensors. Comment-out any sensors you are not using.
     print("Initialising PiicoDev modules")
-    plant.attach('BME280')            # Atmospheric Sensor
-    plant.attach('ENS160')            # Air-Quality Sensor
+    plant.attach('BME280')              # Atmospheric Sensor
+    plant.attach('ENS160')              # Air-Quality Sensor
 #     plant.attach('VEML6040')          # Colour Sensor
-    plant.attach('VEML6030')          # Ambient Light Sensor
+    plant.attach('VEML6030')            # Ambient Light Sensor
 #     plant.attach('VL53L1X')           # Laser Distance Sensor
 #     plant.attach('LIS3DH')            # 3-Axis Accelerometer
 #     plant.attach('QMC6310')           # 3-Axis Magnetometer
 
     print("")
-
+    
     ### Step 2: Collect some data to log
     soil_moisture = plant.measure_soil()
     voltage = plant.measure_system_voltage()
+
+    ### Step 3: Run the pump if plant requires water. This function uses soil moisture to decide whether to run the pump or not.
+    pump_running_seconds = plant.run_pump_control()
+
+    # Print some debugging information
+    print(f'Moisture {soil_moisture:5.2f}%    Pump Time {pump_running_seconds:5.2f}s')
+
 
 #     Ambient Light Sensor VEML6030
     lux = plant.VEML6030_light()
@@ -53,15 +74,8 @@ while True:
 #     flux = plant.QMC6310_flux()
 #     polar = plant.QMC6310_polar()
 
-
-    ### Step 3: Run the pump if plant requires water. This function uses soil moisture to decide whether to run the pump or not.
-    pump_running_seconds = plant.run_pump_control()
-
-    # Print some debugging information
-    print(f'Moisture {soil_moisture:5.2f}%    Pump Time {pump_running_seconds:5.2f}s')
-
     ### Step 4: Log the data to a file
-    period_minutes = 20 # The chosen interval time on the Makerverse Nano Power Timer
+    period_minutes = 10 # The chosen interval time on the Makerverse Nano Power Timer
     file_name = 'log.txt'
 
     # These are the labels that appear at the top of each data column in the log.txt file
@@ -77,7 +91,6 @@ while True:
     heading_tvoc = 'TVOC'
     heading_eco2 = 'eCO2'
     data_heading = [heading_time,heading_moisture, heading_pump, heading_voltage, heading_light, heading_temp, heading_pres, heading_humi, heading_aqi, heading_tvoc, ] # The heading that will appear at the top of the log file
-
 
     logfile = DataLogger(file_name, data_heading, period_minutes) # Open the log file, and write the data_heading if the file was just created.
     timestamp = logfile.last_timestamp + period_minutes # get the most recent timestamp
@@ -97,8 +110,17 @@ while True:
         }
 
     logfile.log_data(data)
-        
-    print(f'Sleeping...')
+    print(f'Data saved: {data}')
+    upload_data = f"plant,sensor_id=RPI2W moisture={soil_moisture},pump={pump_running_seconds},voltage={voltage},lux={lux},temp={temperature_C},pressure={pressure_Pa},humidity={humidity_RH},aqi={AQI.value},tvoc={TVOC},eco2={eCO2.value} {current_time}000000000"
+    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': f'Token {secrets.INFLUXDB_API_KEY}'}
+    print(f"Uploaded data: {upload_data}")
+    
+    try:
+        response = urequests.post(secrets.INFLUXDB_ENDPOINT, headers=headers, data=upload_data.encode())
+    except Exception as e:
+        print("Error sending data to InfluxDB: ", e)
+    print(f'Data uploaded')
+    
     
     ### Step 5: Signal to the Makerverse Nano Power Timer that we are DONE!
     # This removes power from the project until the next timer interval
@@ -108,4 +130,7 @@ while True:
     # Instead we can just insert a delay. When powered only by batteries, this code will never run.
     sleep_ms(round(1000*60*period_minutes))
     
+
+
+
 
